328fe3413265d867fd5d1469d29aa2107512fba0
lfnaranjo11 <35932426+lfnaranjo11@users.noreply.github.com>
2018-09-11 23:10:43 -0500

    Update producer.py

--- producer.py
+++ producer.py
@@ -12,9 +12,7 @@ while True:
         producer.send('bogota-usaquen-longitud1-latitud1', {'time': time.strftime("%X"), 'measurement': 'sitio', 
                                         'value': round(uniform(20, 25),1), 'unit': 'cord', 'place': 'bogota'})
         producer.send('bogota-cahpinero-longitud3-latitud3', {'time': time.strftime("%X"), 'measurement': 'sitio', 
-                                        'value': round(uniform(20, 25),1), 'unit': 'cord', 'place': 'bogota'})
-         producer.send('mexico-condesa-longitud2-latitud2', {'time': time.strftime("%X"), 'measurement': 'sitio', 
-                                        'value': round(uniform(20, 25),1), 'unit': 'cord', 'place': 'mexico'}) 
+                                        'value': round(uniform(20, 25),1), 'unit': 'cord', 'place': 'bogota'}) 
         producer.send('lima-sanfernando-longitud4-latitud4', {'time': time.strftime("%X"), 'measurement': 'sitio', 
                                         'value': round(uniform(20, 25),1), 'unit': 'cord', 'place': 'lima'})                              
 
